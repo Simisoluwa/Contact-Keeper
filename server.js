c139e1b627be101d5ca0ec31795193a3ddccc80f
Simisoluwa <Adeyemo1882@gmail.com>
2019-08-05 02:37:54 +0100

    The full stack react contact keeper

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -15,9 +16,23 @@ app.get('/', (req, res) => {
     res.json({ msg: 'Welcome to the contact keeper app'});
 });
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token');
+    next();
+});
+
 //Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client','build','index.html')));
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
